b469392bb873c45ae72322def919d444b5e3d911
Grenzen <grenzentur@rambler.ru>
2021-08-24 17:54:15 +0300

    Updates

---
+++ precache-manifest.c12f46682e976a894136a48fb2da3ac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dddef6cf4febe6ef88de3b0bf3aac51a",
+    "url": "/to-do-list-incubator/index.html"
+  },
+  {
+    "revision": "9855e71ae9a12c82b558",
+    "url": "/to-do-list-incubator/static/css/main.8b6f52f3.chunk.css"
+  },
+  {
+    "revision": "5ba16cf6c06292ada0e2",
+    "url": "/to-do-list-incubator/static/js/2.5bcb142d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/to-do-list-incubator/static/js/2.5bcb142d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9855e71ae9a12c82b558",
+    "url": "/to-do-list-incubator/static/js/main.dd1bfa27.chunk.js"
+  },
+  {
+    "revision": "3ea6e355957cdd06eae0",
+    "url": "/to-do-list-incubator/static/js/runtime-main.156cf9dc.js"
+  }
+]);
